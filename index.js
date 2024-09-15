57aa59b4e43aa51e6e06c731a29ad763285d9636
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-09-15 13:42:18 +0530

    PUSH request handled

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const fs = require("fs");
+
 PORT = 8000;
 users = require("./MOCK_DATA.json");
 
 app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: false}));
 
 //Routes
 //html SSR (Server Side Rendering)
@@ -33,9 +38,21 @@ app                             //if on same URL multiple routes have to be crea
         return res.json({status:"Pending"});
     })
 
-app.put("/api/users", (req, res)=>{
-    return res.json({status:"Pending"});
-})
+
+app.post("/api/users", (req, res)=>{
+    const body = req.body;
+    users.push({
+        id: users.length + 1,
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        job_title: body.job_title
+    })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {     //stringify = converting object to string
+        return res.json({status: "success", id: users.length});
+    });
+});
 
 
 app.listen(PORT, () => console.log("Server Started"));
